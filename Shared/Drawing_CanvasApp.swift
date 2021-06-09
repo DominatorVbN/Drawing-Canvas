5de52d60445bd8eb88991f81deed3f6c7a046b30
DominatorVbN <voilentbynature@gmail.com>
2021-06-10 01:05:45 +0530

    Initial Commit

---
+++ Shared/Drawing_CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_CanvasApp.swift
+//  Shared
+//
+//  Created by Amit Samant on 10/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
